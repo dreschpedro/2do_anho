7617d74689a9be65f63a855658ddda5cc0ab530d
pedrodresch999 <99pedrodresch@gmail.com>
2022-02-11 19:42:21 -0300

    primer año

---
+++ 1er_anho/programacion1/ejercicios/58-64 matriz/58/ejer58nuevo.pas
@@ -0,0 +1,23 @@
+{Desarrolle un programa en Pascal que
+-permita cargar el menú de almuerzo y cena de lunes a viernes,
+-luego el usuario ingresa un día y el programa le devuelve el menú correspondiente a ese día}
+
+program ejerc58nuevo;
+
+uses Crt;
+
+type
+	menu=record
+		almuerzo: string[20];
+		cena: string[20];
+	end;
+
+procedure cargar;
+	begin
+
+	end;
+
+begin
+
+	readln;
+end.
