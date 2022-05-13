57d187416785502794e66c27cd5ceae30665ad30
pedrodresch999 <99pedrodresch@gmail.com>
2022-05-13 15:07:53 -0300

    ideconfig

--- IDEconfig/git config/aliases.sh
+++ IDEconfig/git config/aliases.sh
@@ -7,10 +7,11 @@ alias ll='ls -l'
 alias cls='clear'
 alias cll='clear'
 alias gst='git status'
-alias gpo='git push origin'
-alias gplo='git pull origin'
+alias gp='git push'
 alias gcm='git commit -m'
 alias gad='git add .'
+alias gam='git commit -am'
+alias nn='nodemon'
 
 case "$TERM" in
 xterm*)
@@ -19,7 +20,7 @@ xterm*)
 	# when run inside `mintty`.
 	for name in node ipython php php5 psql python2.7
 	do
-		case "$(type -p "$name".exe 2>/dev/null)" in 
+		case "$(type -p "$name".exe 2>/dev/null)" in
 		''|/usr/bin/*) continue;;
 		esac
 		alias $name="winpty $name.exe"
--- IDEconfig/git config/git-prompt.sh
+++ IDEconfig/git config/git-prompt.sh
@@ -9,14 +9,14 @@ if test -f ~/.config/git/git-prompt.sh
 then
 	. ~/.config/git/git-prompt.sh
 else
-	PS1='\[\033]0;GitBash $PWD\007\]' # set window title
+	PS1='\[\033]0;$PWD\007\]' # set window title
 	PS1="$PS1"'\n'                 # new line
 	PS1="$PS1"'\[\033[32m\]'       # change to green
 	PS1="$PS1"'\u@\h '             # user@host<space>
 	#PS1="$PS1"'\[\033[35m\]'       # change to purple
 	#PS1="$PS1"'$MSYSTEM '          # show MSYSTEM
 	PS1="$PS1"'\[\033[33m\]'       # change to brownish yellow
-	PS1="$PS1"'\w'                 # current working directory
+	#PS1="$PS1"'\w'                 # current working directory
 	if test -z "$WINELOADERNOEXEC"
 	then
 		GIT_EXEC_PATH="$(git --exec-path 2>/dev/null)"
