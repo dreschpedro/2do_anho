1b82c4b8914891b3b44f4ee08d67a9dcd665dd3b
pedrodresch999 <99pedrodresch@gmail.com>
2022-06-13 11:05:31 -0300

    command lines OS, 3th TP & bd_banco for DB

--- 2do_anho/7sist_Base_datos/4_BD_MySQL/1_Practico_InstruccionesDDL/2 BD BANCO/prestamo.sql
+++ 2do_anho/7sist_Base_datos/4_BD_MySQL/1_Practico_InstruccionesDDL/2 BD BANCO/prestamo.sql
@@ -1,7 +1,7 @@
 CREATE TABLE prestamo (
 num_prestamo INT NOT NULL AUTO_INCREMENT,
 cod_sucursal INT NOT NULL,
-importe INT NOT NULL,
+importe FLOAT NOT NULL,
 PRIMARY KEY (num_prestamo),
-UNIQUE uk_cod_sucursal (cod_sucursal)
+FOREIGN KEY fk_cod_sucursal (cod_sucursal) REFERENCES sucursal (cod_sucursal)
 )
