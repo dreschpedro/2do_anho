81e6b95d6d6d8961ca7d62d3945f3b82c9a6a38a
pedrodresch999 <99pedrodresch@gmail.com>
2022-06-23 16:53:38 -0300

    ejerc 9 prog

--- 2do_anho/7sist_Base_datos/4_BD_MySQL/2_Practico_Instrucciones_DML/24_prom_ped_cat.sql
+++ 2do_anho/7sist_Base_datos/4_BD_MySQL/2_Practico_Instrucciones_DML/24_prom_ped_cat.sql
@@ -1,6 +1,6 @@
 -- LISTADO DEL PROMEDIO DE PEDIDOS POR CATEGORIA DE PRODUCTO
 
-SELECT categorías.`idcategoría` AS categoria, AVG(pedidos.idpedido) AS pedidos_por_cat
+SELECT categorías.`nombrecategoría` AS categoria, ROUND(AVG(detalles_de_pedidos.cantidad), 3)  AS pedidos_por_cat
 FROM pedidos
 JOIN detalles_de_pedidos ON pedidos.idpedido = detalles_de_pedidos.idpedido
 JOIN productos ON productos.idproducto = detalles_de_pedidos.idproducto
