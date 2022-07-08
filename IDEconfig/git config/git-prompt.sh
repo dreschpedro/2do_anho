f4d910babd346300a60cb4afdb7c7c4ab8e757ea
pedrodresch999 <99pedrodresch@gmail.com>
2022-07-07 23:07:37 -0300

    agregado todo el material nuevamente

--- IDEconfig/git config/git-prompt.sh
+++ IDEconfig/git config/git-prompt.sh
@@ -12,11 +12,11 @@ else
 	PS1='\[\033]0;$PWD\007\]' # set window title
 	PS1="$PS1"'\n'                 # new line
 	PS1="$PS1"'\[\033[32m\]'       # change to green
-	PS1="$PS1"'\u@\h '             # user@host<space>
+	#PS1="$PS1"'\u@\h '             # user@host<space>
 	#PS1="$PS1"'\[\033[35m\]'       # change to purple
 	#PS1="$PS1"'$MSYSTEM '          # show MSYSTEM
 	PS1="$PS1"'\[\033[33m\]'       # change to brownish yellow
-	PS1="$PS1"'\w'                 # current working directory
+	#PS1="$PS1"'\w'                 # current working directory
 	if test -z "$WINELOADERNOEXEC"
 	then
 		GIT_EXEC_PATH="$(git --exec-path 2>/dev/null)"
